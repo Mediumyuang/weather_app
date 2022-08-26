8af9d54096b5275b7a9acb87006abc8d1c9b30d9
Mediumyaung <112119912+Mediumyuang@users.noreply.github.com>
2022-08-26 12:02:58 +0200

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-
-
-// ignore_for_file: unused_element
-
 import 'package:flutter/material.dart';
 
 
 void main() {
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
